7e1adca30a18deb3590e323fac96371f299a930c
Stepan <polyakovs@list.ru>
2022-05-12 21:19:10 +0700

    add if and work on getComputerChange

--- src/RPSv2/GameLogic.java
+++ src/RPSv2/GameLogic.java
@@ -3,10 +3,14 @@ package RPSv2;
 import java.util.Random;
 import java.util.Scanner;
 
+import static RPSv2.GameField.PAPER;
+
 public class GameLogic {
+
     Field field = new Field();
 
     public void start() {
+        System.out.println("It's game \"STONE\", \"SCISSORS\", \"PAPER\".\n Make you choice:");
         field.setHuman(getHumanChange());
         field.setComputer(getComputerChange());
         winnerChange(field.getHuman(),field.getComputer());
@@ -14,8 +18,37 @@ public class GameLogic {
 
     private void winnerChange(GameField humanValue, GameField compValue) {
         //сравнение твоеЗнач с компЗнач
-        GameField виннер = присвоить результат сравнения;
-        System.out.println("Победил" + виннер);
+        String winner = new String();
+
+        if (humanValue == GameField.SCISSORS && compValue ==GameField.PAPER) {
+            winner = "Human";
+        }
+        if (humanValue == GameField.SCISSORS && compValue ==GameField.SCISSORS) {
+            winner = "не определен. Ничья.";
+        }
+        if (humanValue == GameField.SCISSORS && compValue ==GameField.STONE) {
+            winner = "Computer";
+        }
+        if (humanValue == GameField.PAPER && compValue ==GameField.PAPER) {
+            winner = "не определен. Ничья.";
+        }
+        if (humanValue == GameField.PAPER && compValue ==GameField.SCISSORS) {
+            winner = "Computer";
+        }
+        if (humanValue == GameField.PAPER && compValue ==GameField.STONE) {
+            winner = "Human";
+        }
+        if (humanValue == GameField.STONE && compValue ==GameField.PAPER) {
+            winner = "Computer";
+        }
+        if (humanValue == GameField.STONE && compValue ==GameField.SCISSORS) {
+            winner = "Human";
+        }
+        if (humanValue == GameField.STONE && compValue ==GameField.STONE) {
+            winner = "не определен. Ничья.";
+        }
+
+        System.out.println("Победитель " + winner);
     }
 
 
