66871cc6142d46012dfd3283987031cf27d0eb1d
Stepan <polyakovs@list.ru>
2022-05-14 23:37:45 +0700

    add Study of Pestehin!!) First is done!!

---
+++ src/addStudy/Array.java
@@ -0,0 +1,16 @@
+package addStudy;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[]{ 3,2,3,1,5 };
+        int j;
+        int n = 0;
+        for (int i = 0; i<5; i++) {
+            j = array[n];
+            System.out.println(j);
+            n++;
+        }
+
+
+    }
+}
