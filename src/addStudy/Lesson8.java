80cdedbd7f8d021df876caf9dc1b8ee5549ddfb9
Stepan <polyakovs@list.ru>
2022-05-15 23:27:16 +0700

    Start lesson 8

--- src/addStudy/Lesson8.java
+++ src/addStudy/Lesson8.java
@@ -1,4 +1,15 @@
 package addStudy;
 
 public class Lesson8 {
+    public static void main(String[] args) {
+        Go go = new Go();
+        go.str();
+    }
+}
+    class Go {
+        void str () {
+        String str1 = "I love dota and java, maybe...";
+
+
+    }
 }
