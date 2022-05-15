102454f43d5786d9e80075cc490e78177bd3155a
Stepan <polyakovs@list.ru>
2022-05-15 22:36:36 +0700

    Lesson 6 is patch Lesson 7 is done

--- src/addStudy/Lesson6.java
+++ src/addStudy/Lesson6.java
@@ -2,14 +2,23 @@ package addStudy;
 
 public class Lesson6 {
     public static void main(String[] args) {
-        int array[] = new int[] {15,20,75,10,83,11};
-        int search = 10;
-
-        for (int num : array) {
-            if (num == search) {
-                continue;
-            }
-            System.out.println(num);
-        }
+        Start1 start1 = new Start1();
+        start1.starter();
     }
 }
+        class Start1 {
+
+            void starter() {
+                int array[] = new int[]{15, 20, 75, 10, 83, 11};
+                int search = 10;
+
+                for (int num : array) {
+                    if (num == search) {
+                        continue;
+                    }
+                    System.out.println(num);
+                }
+
+            }
+        }
+
---
+++ src/addStudy/Lesson7.java
@@ -0,0 +1,19 @@
+package addStudy;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.Box(5);
+
+    }
+}
+class Start{
+     double Box(int i) {
+        double j = (double) i;
+         System.out.println(j);
+        return j;
+     }
+
+
+}
+
