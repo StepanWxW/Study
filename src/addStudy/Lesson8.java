508e9d00ebab47bcad66c7974c7b306d13c064f4
Stepan <polyakovs@list.ru>
2022-05-16 16:30:25 +0700

    lesson 8 save, but work else

--- src/addStudy/Lesson8.java
+++ src/addStudy/Lesson8.java
@@ -2,14 +2,95 @@ package addStudy;
 
 public class Lesson8 {
     public static void main(String[] args) {
-        Go go = new Go();
-        go.str();
+        A a = new A();
+        a.str();
+        a.str2("I love dota and java, maybe...");
+//        go.str3("I love dota and java, maybe...");
+        B b = new B();
+        b.str("I love dota and java, maybe...");
+        b.str2();
+        C c = new C();
+        c.str();
     }
 }
-    class Go {
-        void str () {
-        String str1 = "I love dota and java, maybe...";
+    class A {
+        void str() {
+            StringBuffer str1 = new StringBuffer("I love dota and java, maybe...");
+            StringBuffer str2 = str1.reverse();
+            System.out.println(str2);
+        }
+        void str2(String str) {
 
 
+            String result ="";
+            for (int i =0 ; i<str.length() ; i++){
+                result = str.charAt(i) +result;
+            }
+
+            System.out.println(result);
+
+        }
+//        void str3(String str) {
+//
+//            String x = "I love dota";
+//            String result ="";
+//            for (int num : ) {
+//                result = str.charAt(i) +result;
+//            }
+//
+//            System.out.println(result);
+
+}
+class B {
+    //    void str(String str) {
+//        String result = "";
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == ',') {
+//                for (int j = 0; i > str.charAt(i); j++) {
+//                    result = str.charAt(j) + result;
+//                    i++;
+//                }
+//            }System.out.println(result);
+//
+//        }
+//
+//
+//    }
+    void str(String str) {
+        System.out.println(str.substring(20));
+    }
+
+    //    void str2() {
+//        String x ="I love dota and java, maybe...";
+//        String result ="";
+//        for (int i =0; i<x.length(); i++) {
+//            if (x.charAt(i) == ',') {
+//                System.out.println(x.charAt(x.charAt(i)<x.length()));
+//            }
+//        }
+//
+//    }
+//}
+    void str2() {
+        String str = "I love dota and java, maybe...";
+        String resultStr = str.substring(str.indexOf(',') + 1);
+        System.out.println(resultStr);
     }
 }
+class C {
+    void str() {
+        String str = "I love dota and java, maybe...";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            }
+
+        }
+        System.out.println(chars);
+    }
+}
+
