4274ab6a9ba8d61fd2b2b11b1b827f7bd6742c5d
Stepan <polyakovs@list.ru>
2022-05-18 13:43:18 +0700

    chapter 9 add many)

---
+++ src/chapter9/MyClass.java
@@ -0,0 +1,13 @@
+package chapter9;
+
+public class MyClass implements B1{
+    public void meth1() {
+        System.out.println("Meth1 work");
+    }
+    public void meth2() {
+        System.out.println("Meth2 is work");
+    }
+    public void meth3() {
+        System.out.println("Meth3 is work");
+    }
+}
