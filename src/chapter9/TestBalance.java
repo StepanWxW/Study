7b5dd2c07a4b714648f2d7d9e920ca6869752b02
Stepan <polyakovs@list.ru>
2022-05-18 11:02:12 +0700

    chapter 9 work

---
+++ src/chapter9/TestBalance.java
@@ -0,0 +1,11 @@
+package chapter9;
+
+import chapter9.mypack.*;
+
+    public class TestBalance {
+        public static void main(String[] args) {
+            Balance test = new Balance("Text", -353);
+            test.show();
+        }
+    }
+
--- src/chapter9/mypack/AccountBalance.java
+++ src/chapter9/mypack/AccountBalance.java
@@ -1,19 +1,5 @@
 package chapter9.mypack;
 
-class Balance {
-    String name;
-    double bal;
-    Balance(String n, double b) {
-        name = n;
-        bal = b;
-    }
-    void show() {
-        if(bal<0) {
-            System.out.print("--> ");
-            System.out.println(name + ": $" + bal);
-        }
-    }
-}
 public class AccountBalance {
     public static void main(String[] args) {
         Balance current[] = new Balance[3];
