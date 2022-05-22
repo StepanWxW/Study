be5a15577ccbee6932153458d768d46cb4f07971
Stepan <polyakovs@list.ru>
2022-05-22 23:02:16 +0700

    commit)

--- src/addStudy/OOP/new10a/Animal.java
+++ src/addStudy/OOP/new10a/Animal.java
@@ -1,26 +1,28 @@
 package addStudy.OOP.new10a;
 
+import static java.lang.System.out;
+
 class Animal {
     private String nameClass;
     private String voice;
     private String head;
     private int age;
     static {
-        System.out.println("static block");
+        out.println("static block");
     }
     {
         voice = "abc";
-        System.out.println("non static block");
+        out.println("non static block");
     }
     Animal() {
-        System.out.println("constructor block" + voice);
+        out.println("constructor block" + voice);
     }
 
     void vote() {
         System.out.println("Voice animal: " + voice);
     }
     String vote(String name) {
-        System.out.println("Voice animal: " + voice);
+        out.println("Voice animal: " + voice);
         return name;
     }
 }
--- src/addStudy/OOP/new10a/Start.java
+++ src/addStudy/OOP/new10a/Start.java
@@ -5,5 +5,14 @@ public class Start {
         Dog dog = new Dog("asad");
         System.out.println(dog);
         System.out.println(dog.toString());
+        Animal cow = new Cow();
+        Animal cat = new Cat();
+        animal(dog);
+        animal(cow);
+        animal(cat);
+
+    }
+    public static void animal(Animal animal) {
+        animal.vote();
     }
 }
