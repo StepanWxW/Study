1b63181435ad2428d90c5c3e0bb4e8e9185cbc70
Stepan <polyakovs@list.ru>
2022-05-25 15:41:30 +0700

    LinkedListDemo

---
+++ src/chapter19/LinkedListDemo.java
@@ -0,0 +1,27 @@
+package chapter19;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("F");
+        ll.add("B");
+        ll.add("D");
+        ll.add("E");
+        ll.add("C");
+        ll.addLast("Z");
+        ll.addFirst("A");
+        ll.add(1,"A2");
+        System.out.println("Исходное содержимое связного списка ll: " + ll);
+        ll.remove("F");
+        ll.remove(2);
+        System.out.println("Содержимое связного списка ll после удаления элементов: " + ll);
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println("Содержимое связного списка ll после удаления первого и последнего: " + ll);
+        String val = ll.get(2);
+        ll.set(2,val + " изменено");
+        System.out.println("Содержимое связного списка ll после изменения: " + ll);
+    }
+}
