3e8a777603f026b1a7a8207b37f415c4ffa4a7f3
Stepan <polyakovs@list.ru>
2022-05-26 17:26:34 +0700

    add TreeMapDemo

---
+++ src/chapter19/TreeMapDemo.java
@@ -0,0 +1,24 @@
+package chapter19;
+import java.util.*;
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<String, Double> tm = new TreeMap<String, Double>();
+        tm.put("Джон Доу", new Double(3434.34));
+        tm.put("Том Смитт", new Double(123.23));
+        tm.put("Джейн Бейкер", new Double(1378.00));
+        tm.put("Тод Холл", new Double(99.22));
+        tm.put("Ральф Смитт", new Double(-19.08));
+
+        Set<Map.Entry<String, Double>> set = tm.entrySet();
+        for(Map.Entry<String,Double> me : set) {
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+        System.out.println();
+
+        double balance = tm.get("Джон Доу");
+        tm.put("Джон Доу", balance + 1000);
+        System.out.println("Новый остаток на счете Джон Доу: " + tm.get("Джон Доу"));
+
+    }
+}
