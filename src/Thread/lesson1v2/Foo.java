75db0c08f3fd2c8e2d6f9c05cf17498af1dd3fdc
Stepan Polykov <polyakovs@list.ru>
2022-06-30 12:41:27 +0700

    add Threadlesson1v2 is work V2!!!

--- src/Thread/lesson1v2/Foo.java
+++ src/Thread/lesson1v2/Foo.java
@@ -1,13 +1,45 @@
 package Thread.lesson1v2;
 
 public class Foo {
+    private volatile int orderNum = 1;
     public void first() {
-        System.out.print("first");
+        synchronized (this) {
+            while (orderNum != 1) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            orderNum = 2;
+            System.out.print("first");
+            notifyAll();
+        }
     }
     public void second() {
-        System.out.print("second");
+        synchronized (this) {
+            while (orderNum != 2) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            orderNum = 3;
+            System.out.print("second");
+            notifyAll();
+        }
     }
     public void third() {
+            synchronized (this) {
+                while (orderNum != 3) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        }
         System.out.print("third");
     }
 }
--- src/Thread/lesson1v2/Work.java
+++ src/Thread/lesson1v2/Work.java
@@ -1,59 +1,12 @@
 package Thread.lesson1v2;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Work {
     Foo foo = new Foo();
-    private static volatile int orderNum = 1;
     public void start(){
-    Thread thread1 = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            synchronized (this) {
-                while (orderNum != 1) {
-                    try {
-                        wait(1);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                foo.first();
-                orderNum = 2;
-            }
-        }
-    });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (this) {
-                    while (orderNum != 2) {
-                        try {
-                            wait(1);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    foo.second();
-                    orderNum = 3;
-                }
-            }
-        });
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (this) {
-                    while (orderNum != 3) {
-                        try {
-                            wait(1);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                        foo.third();
-                    }
-                }
-        });
+    Thread thread1 = new Thread(() -> foo.first());
+        Thread thread2 = new Thread(() -> foo.second());
+        Thread thread3 = new Thread(() -> foo.third());
         thread3.start();
         thread2.start();
         thread1.start();
